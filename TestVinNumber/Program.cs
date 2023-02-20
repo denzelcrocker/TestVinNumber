868472ea0d468fa2b6f7ec380b743f9b4bc524ef
Дениска Зеленцов <zelentsov36@gmail.com>
2023-02-20 11:45:16 +0300

    В файлах проекта прикреплен документ с тест - кейсами

--- TestVinNumber/Program.cs
+++ TestVinNumber/Program.cs
@@ -12,7 +12,7 @@ namespace TestVinNumber
         {
             VIN_LIB.VIN vin = new VIN_LIB.VIN();
             Console.WriteLine($"Достоверность номера - {vin.CheckVIN("1ZVHT82H485113456")}");
-            Console.WriteLine($"Географическая зона - {vin.GetVINCountry("1ZVHT82H485113456")}");
+            Console.WriteLine($"Географическая зона - {vin.GetVINCountry("Z8NAJL00050A66148")}");
             Console.ReadKey();
         }
     }
